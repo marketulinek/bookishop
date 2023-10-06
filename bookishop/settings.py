57e6467c458764a18410f297f1052e3d22663a91
Markéta <100078291+marketulinek@users.noreply.github.com>
2023-10-06 23:25:48 +0200

    Update settings.py

--- bookishop/settings.py
+++ bookishop/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': config('DB_NAME', default='postgres'),
         'USER': config('DB_USER', default='postgres'),
         'PASSWORD': config('DB_PASSWORD', default='postgres'),
-        'HOST': config('DB_HOST', default='localhost'),
+        'HOST': config('DB_HOST', default='db'),
         'PORT': config('DB_PORT', default='5432', cast=int),
     }
 }
