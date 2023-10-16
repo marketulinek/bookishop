2743ca170c7249d664f2d45a269daa76d91d9ba2
MarketaH <habranova.marketa@gmail.com>
2023-10-16 20:37:10 +0200

    Add book list slider to the homepage to show new books

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,11 @@
 from django.views.generic import TemplateView
+from books.models import Book
 
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['new_books'] = Book.objects.order_by('-published_at')[:10]
+        return context
