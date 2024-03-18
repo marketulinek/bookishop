3a0a542df409ebea4bf776ce07c3b2bd77c5eba1
MarketaH <habranova.marketa@gmail.com>
2024-03-18 21:09:15 +0100

    Add the option for logged in users to add a book to their wishlist from its detail page

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import SignupPageView
+from .views import SignupPageView, add_to_wishlist
 
 urlpatterns = [
     path('signup/', SignupPageView.as_view(), name='signup'),
+
+    path('wishlist/add/<slug:slug>/', add_to_wishlist, name='add_to_wishlist'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,40 @@
+import json
+
+from django.db.utils import IntegrityError
+from django.http import HttpResponse
 from django.urls import reverse_lazy
+from django.utils.translation import gettext as _
 from django.views import generic
+from django.views.decorators.http import require_http_methods
 
 from .forms import CustomUserCreationForm
+from .models import Wishlist
+from books.models import Book
 
 
 class SignupPageView(generic.CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
+
+@require_http_methods(['POST'])
+def add_to_wishlist(request, slug):
+
+    if not request.user.is_authenticated:
+        toast = ('danger', _('You have to be logged in.'))
+    else:
+        try:
+            book = Book.objects.get(slug=slug)
+            Wishlist(user=request.user, book=book).save()
+            toast = ('success', _('The book has been added to your wishlist.'))
+        except Book.DoesNotExist:
+            toast = ('danger', _('The book does not exist!'))
+        except IntegrityError:
+            toast = ('warning', _('The book is already on your wishlist.'))
+        except Exception:
+            toast = ('danger', _('An error occurred!'))
+
+    return HttpResponse(status=204, headers={'HX-Trigger': json.dumps({
+        'showMessage': {'level': toast[0], 'message': toast[1]}
+    })})
