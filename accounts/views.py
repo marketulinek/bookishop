0426e8e48a6baf007f6d9b2aae75631aed49215c
MarketaH <habranova.marketa@gmail.com>
2024-05-04 08:59:51 +0200

    Modify status codes to be more appropriate

--- accounts/views.py
+++ accounts/views.py
@@ -33,19 +33,24 @@ def add_to_wishlist(request, slug):
 
     if not request.user.is_authenticated:
         toast = ('danger', _('You have to be logged in.'))
+        status_code = 401
     else:
         try:
             book = Book.objects.get(slug=slug)
             Wishlist(user=request.user, book=book).save()
             toast = ('success', _('The book has been added to your wishlist.'))
-        except Book.DoesNotExist:
-            toast = ('danger', _('The book does not exist!'))
+            status_code = 200
         except IntegrityError:
             toast = ('warning', _('The book is already on your wishlist.'))
+            status_code = 200
+        except Book.DoesNotExist:
+            toast = ('danger', _('The book does not exist.'))
+            status_code = 400
         except Exception:
-            toast = ('danger', _('An error occurred!'))
+            toast = ('danger', _('An error occurred.'))
+            status_code = 400
 
-    return HttpResponse(status=200, headers={'HX-Trigger': json.dumps({
+    return HttpResponse(status=status_code, headers={'HX-Trigger': json.dumps({
         'showMessage': {'level': toast[0], 'message': toast[1]}
     })})
 
@@ -55,16 +60,20 @@ def remove_from_wishlist(request, slug):
 
     if not request.user.is_authenticated:
         toast = ('danger', _('You have to be logged in.'))
+        status_code = 401
     else:
         try:
             book = Book.objects.get(slug=slug)
             Wishlist.objects.get(user=request.user, book=book).delete()
             toast = ('success', _('The book has been removed from your wishlist.'))
+            status_code = 200
         except Book.DoesNotExist:
-            toast = ('danger', _('The book does not exist!'))
+            toast = ('danger', _('The book does not exist.'))
+            status_code = 400
         except Exception:
-            toast = ('danger', _('An error occurred!'))
+            toast = ('danger', _('An error occurred.'))
+            status_code = 400
 
-    return HttpResponse(status=200, headers={'HX-Trigger': json.dumps({
+    return HttpResponse(status=status_code, headers={'HX-Trigger': json.dumps({
         'showMessage': {'level': toast[0], 'message': toast[1]}
     })})
