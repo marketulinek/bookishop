febe8b2203fdbb3dc2a3462a53193870f6153f5b
MarketaH <habranova.marketa@gmail.com>
2024-08-19 22:45:47 +0200

    Move wishlist migrations from accounts app to wishlist app

--- accounts/models.py
+++ accounts/models.py
@@ -9,14 +9,14 @@ class CustomUser(AbstractUser):
 
 
 class Wishlist(models.Model):
-    user = models.ForeignKey(CustomUser, related_name='users', on_delete=models.CASCADE)
-    book = models.ForeignKey(Book, related_name='books', on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, related_name='_users', on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, related_name='_books', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(fields=['user', 'book'], name='unique_user_book')
-        ]
+    # class Meta:
+    #     constraints = [
+    #         models.UniqueConstraint(fields=['user', 'book'], name='unique_user_book')
+    #     ]
 
     def __str__(self):
         return f'{self.user} likes {self.book}'
