fd633dfe1d5fa00ab8ad033f04cac1a4485ab76d
MarketaH <habranova.marketa@gmail.com>
2024-08-25 10:38:20 +0200

    Fixed the wrong operator

--- bookishop/urls.py
+++ bookishop/urls.py
@@ -25,7 +25,7 @@ urlpatterns += i18n_patterns(
 if settings.DEBUG:
     import debug_toolbar
 
-    urlpatterns += [
+    urlpatterns = [
         path('__debug__/', include(debug_toolbar.urls)),
     ] + urlpatterns
 
